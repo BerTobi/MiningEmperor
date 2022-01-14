6745e738bbef35d2c3c02e5d57cb6a8de1262cd6
Tobix192 <tobiasbersia@gmail.com>
2022-01-14 13:26:45 -0300

    Save game test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var money = 0;
+var MpT = 0;
 var MpS = 0;
 
 var minerCost = 15;
@@ -45,27 +46,63 @@ function buyPump(){
     }
 }
 
-function updateMoneyPerSecond(){
-    MpS = miners + saws * 5 + pumps * 50;
+function updateMoneyPerTick(){
+    MpT = miners * 0.02 + saws * 0.1 + pumps * 1;
+    MpS = MpT * 50;
     updateScreen()
 }
 
 function updateScreen(){
 
-    document.getElementById("money").innerHTML = money;
+    document.getElementById("money").innerHTML = displayMoney;
     document.getElementById("miners").innerHTML = miners;
     document.getElementById("minerCost").innerHTML = minerCost;
     document.getElementById("saws").innerHTML = saws;
     document.getElementById("sawCost").innerHTML = sawCost;
     document.getElementById("pumps").innerHTML = pumps;
     document.getElementById("pumpCost").innerHTML = pumpCost;
-    document.getElementById("mps").innerHTML = MpS;
-    document.title = money + " money - Mining Emperor";
+    document.getElementById("mps").innerHTML = displayMpS;
+    document.title = displayMoney + " money - Mining Emperor";
     
 }
 
+function saveGame() {
+    var gameSave = {
+        money: money,
+        minerCost: minerCost,
+        miners: miners,
+        sawCost: sawCost,
+        saws: saws,
+        pumpCost: pumpCost,
+        pumps: pumps
+    };
+    localStorage.setItem("gameSave", JSON.stringify(gameSave));
+}
+
+function loadGame() {
+    var savedGame = JSON.parse(localStorage.getItem("gameSave"));
+    if (typeof savedGame.money !== "undefined") money = savedGame.money;
+    if (typeof savedGame.minerCost !== "undefined") money = savedGame.minerCost; 
+    if (typeof savedGame.miners !== "undefined") money = savedGame.miners; 
+    if (typeof savedGame.sawCost !== "undefined") money = savedGame.sawCost; 
+    if (typeof savedGame.saws !== "undefined") money = savedGame.saws; 
+    if (typeof savedGame.pumpCost !== "undefined") money = savedGame.pumpCost; 
+    if (typeof savedGame.pumps !== "undefined") money = savedGame.pumps; 
+}
+
+window.onload = function() {
+    loadGame();
+    updateScreen();
+}
+
 setInterval(function(){
-    money = money + MpS;
-    updateMoneyPerSecond();
+    money = money + MpT;
+    displayMoney = money.toFixed(0);
+    displayMpS = MpS.toFixed(0);
+    updateMoneyPerTick();
     updateScreen()
-}, 1000);
+}, 20);
+
+setInterval(function(){
+    saveGame();
+}, 30000);
